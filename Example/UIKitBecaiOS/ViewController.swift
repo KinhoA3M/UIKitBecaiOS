2ca18b92dd68afffbbde235710d444fc321a500e
KinhoA3M <erick.a.menezes31@gmail.com>
2021-04-12 14:38:35 -0300

    Commit Módulo

--- Example/UIKitBecaiOS/ViewController.swift
+++ Example/UIKitBecaiOS/ViewController.swift
@@ -7,17 +7,22 @@
 //
 
 import UIKit
+import UIKitBecaiOS
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnAvancar: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        btnAvancar.setTitle(buttonBeca.tituloNormal , for: .normal)
+        
+        btnAvancar.setTitle(buttonBeca.tituloClick, for: .highlighted)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
---
+++ ViewModel/MeuComponene.swift
@@ -0,0 +1,14 @@
+public enum MeuComponenteEnum: String {
+    case Semana = "www.google.com.br"
+    case Dia
+    case Tudo
+}
+
+public struct buttonBeca {
+    public static let tituloNormal = "Avançará"
+    public static let tituloClick = "Clicado"
+}
+
+public class Compxxx: UIView {
+    var teste: String = ""
+}
